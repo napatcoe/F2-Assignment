b99755a00de3a24a6f4876f2e52e7a2cba2b0182
napatcoe <napat.coe@gmail.com>
2016-04-22 13:30:51 +0700

    02 - ออกแบบตัวยาน

---
+++ SpaceShip.java
@@ -0,0 +1,32 @@
+package f2.spw;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class SpaceShip extends Sprite{
+
+	int step = 8;
+	
+	public SpaceShip(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		g.setColor(Color.GRAY);
+		g.fillRect(x+5, y-5, width-10, height);
+		g.setColor(Color.WHITE);
+		g.fillRect(x+10, y-10, width-20, height);
+		
+	}
+
+	public void move(int direction){
+		x += (step * direction);
+		if(x < 0)
+			x = 0;
+		if(x > 400 - width)
+			x = 400 - width;
+	}
+
+}
