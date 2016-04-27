5bb009fb391ceab2ba1516179ac7265279dc64c4
napatcoe <napat.coe@gmail.com>
2016-04-27 14:38:49 +0700

    04 - สร้าง item

--- Item.java
+++ Item.java
@@ -1,1 +1,58 @@
 
+package f2.spw;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import javax.swing.Timer;
+
+public class Item extends Sprite{
+	public static final int Y_TO_FADE = 400;
+	public static final int Y_TO_DIE = 600;
+	
+	private int type = 0;
+	private int step = 10;
+	private boolean alive = true;
+	
+	public Item(int x, int y) {
+		super(x, y, 5, 5);
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		if(y < Y_TO_FADE)
+			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+		else{
+			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 
+					(float)(Y_TO_DIE - y)/(Y_TO_DIE - Y_TO_FADE)));
+		}
+		
+		g.setColor(Color.WHITE);
+		g.fillRect(x,y,width,height);
+		
+	}
+
+	public void proceed(){
+		y += step;
+		if(y > Y_TO_DIE){
+			alive = false;
+		}
+	}
+	
+	public boolean isAlive(){
+		return alive;
+	}
+	
+	public boolean deleteItem(){
+		return alive = false;
+	}
+
+	public void setType(){
+		type = (int)(Math.random()*6);
+	}
+
+	public int getType(){
+		return type;
+	}
+}
